385702c0e45a14219c32a17865e22cc79ba3dc2c
R Srinivasan <rs@toprllc.com>
2023-08-24 19:55:02 -0400

    more std support

---
+++ adalib/src/hex-dump.ads
@@ -0,0 +1,16 @@
+with System;
+with Ada.Text_IO;
+
+package Hex.dump is
+
+   DEFAULT_BLOCK_LENGTH : constant Integer := 32;
+
+   procedure Dump
+     (Adr         : System.Address;
+      Length      : Integer;
+      show_offset : Boolean               := True;
+      bare : Boolean := False;
+      Blocklen    : Integer               := DEFAULT_BLOCK_LENGTH;
+      Outfile     : Ada.Text_IO.File_Type := Ada.Text_IO.Standard_Output);
+
+end Hex.dump;
