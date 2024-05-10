f3125518bab7f1311f721dc27671eba94c8668c3
R Srinivasan <rs@toprllc.com>
2024-05-09 20:35:22 -0400

    circular buffer (fifo) support

---
+++ adalib/src/fifo.adb
@@ -0,0 +1,63 @@
+package body fifo is
+
+   function Create( cap : Integer := 64 ) return Buffer_Type is
+   	result : Buffer_Type(cap);
+   begin
+   	return result ;
+   end Create ;
+
+   procedure Push( buf : in out Buffer_Type; item : Item_Type ) is
+   begin
+
+      if Full(buf)
+      then
+         raise Overflow_Error ;
+      end if ;
+
+      buf.Contents(buf.push_to) := item ;
+      buf.count := @ + 1 ;
+      if buf.pull_from < buf.Contents'First
+      then
+         buf.pull_from := buf.push_to ;
+      end if ;
+
+      if buf.push_to = buf.Contents'last
+      then
+         buf.push_to := buf.Contents'First ;
+      else
+         buf.push_to := @ + 1 ;
+      end if ;
+
+   end Push ;
+
+   procedure Get( buf : in out Buffer_Type; item : out Item_Type ) is
+   begin
+
+      if Empty(buf)
+      then
+         raise Empty_Error ;
+      end if ;
+
+      item := buf.Contents( buf.pull_from );
+      buf.count := @ - 1 ;
+      if buf.pull_from = buf.contents'Last
+      then
+         buf.pull_from := buf.Contents'First ;
+      else
+         buf.pull_from := @ + 1;
+      end if ;
+
+   end Get ;
+
+   function Empty( buf : Buffer_Type ) return Boolean is
+   begin
+      return buf.count = 0 ;
+   end Empty ;
+
+   function Full( buf : Buffer_Type ) return Boolean is
+   begin
+      return buf.count = buf.Contents'Last ;
+   end Full ;
+
+
+end fifo ;
--- examples/revarg/src/revarg.adb
+++ examples/revarg/src/revarg.adb
@@ -3,16 +3,18 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Strings.Unbounded ; use Ada.Strings.Unbounded ;
 with Ada.Command_Line; use Ada.Command_Line;
 with stacks ;
+with fifo ;
 
 procedure Revarg is
    package arglist_Pkg is new stacks (Unbounded_String);
    use arglist_Pkg ;
-
-   revargs : arglist_Pkg.Stack := Create ;
+   package argbuf_Pkg is new fifo (Unbounded_String) ;
 
    content : Unbounded_String ;
-begin
-   Outer_Loop : for i in 1..Argument_Count
+   procedure T1 is   
+      revargs : arglist_Pkg.Stack := Create ;
+   begin
+     Outer_Loop : for i in 1..Argument_Count
    loop
       for j in 1..10
       loop
@@ -32,4 +34,38 @@ begin
       Put_Line(To_String(content)) ;
    end loop ;
 
+   end T1 ;
+
+   procedure T2 is
+      circbuf : argbuf_Pkg.Buffer_Type := argbuf_Pkg.Create(32);
+      count : Integer := 0;
+   begin
+      Outer_Loop : for i in 1..Argument_Count loop
+
+         for j in 1..Argument_Count
+         loop
+            if argbuf_Pkg.Full(circbuf)
+            then
+               Put(count); Put(" elements added to circular buffer"); New_Line ;
+               Exit Outer_Loop ;
+            end if ;
+            argbuf_Pkg.Push(circbuf,To_Unbounded_String(Argument(i)));
+            count := @ + 1 ;
+         end loop ;
+      end loop Outer_Loop;
+      Put_Line("Circular buffer contents ......");
+      count := 1 ;
+      while not argbuf_Pkg.Empty(circbuf)
+      loop
+         Put(count) ; Put(" > ");
+         argbuf_Pkg.Get(circbuf,content);
+         Put_Line(To_String(content));
+         count := @ + 1 ;
+      end loop ;
+
+   end T2 ;
+
+begin
+   T1 ;
+   T2 ;
 end Revarg;
