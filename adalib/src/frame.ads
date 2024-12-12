abad5cef1fab70b7e6ed631c5ba99676ed15beb4
R Srinivasan <rs@toprllc.com>
2024-12-12 11:29:55 -0500

    Unnecessary

--- adalib/src/frame.ads
+++
@@ -1,7 +0,0 @@
-with GNAT.Strings ;
-package frame is
-   type DataItem_Type is abstract tagged record
-      name : GNAT.Strings.String_Access ;
-   end record;
-   type DataItems_Type is array (integer range <>) of access DataItem_Type ;
-end frame ;
