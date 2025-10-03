72b3e3330f3cbd7f4cf64ac66ea7a4bdd8ce4c6e
R Srinivasan <rs@toprllc.com>
2025-10-03 18:05:38 -0400

    now with tags

--- examples/gitrev/src/revisions.ads
+++ examples/gitrev/src/revisions.ads
@@ -1,11 +1,12 @@
 --------------------------------------------
--- Created 2025-10-02 15:17:22
+-- Created 2025-10-03 18:04:17
 --------------------------------------------
 package revisions is
 	dir : constant String := "/Users/rajasrinivasan/Prj/GitHub/toolkit/examples/gitrev" ;
 	version : constant String := "0.0.3" ;
 	repo : constant String := "git@github.com:RajaSrinivasan/toolkit.git" ;
-	commitid : constant String := "bd7755ffe74522ebee143dce23152262d8fa33da" ;
-	abbrev_commitid : constant String := "bd7755f" ;
+	commitid : constant String := "a9cf8a4349f395079e52f360331ba2b85921959f" ;
+	abbrev_commitid : constant String := "a9cf8a4" ;
+	tags : constant String := "0.1.0-dev" ;
 	branch : constant String := "main" ;
 end revisions ;
