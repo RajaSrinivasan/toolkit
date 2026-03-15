f93944589819a94fa61778a87e68a2fe0cbd7f07
R Srinivasan <rs@toprllc.com>
2026-03-15 07:30:06 -0400

    capture github env

--- utilities/build.sh
+++ utilities/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pushd gitrev; alr build; popd
-./bin/gitrev -r 0.2.0 -o revisions .
+./bin/gitrev -r 0.2.0 -eSHA=$GITHUB_SHA,RUN=$GITHUB_RUN_ID:$GITHUB_RUN_NUMBER -o revisions .
 pushd codemd; alr build; popd
