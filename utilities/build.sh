bdc0d91da9a84738256562f284fe8ee8e845b873
R Srinivasan <rs@toprllc.com>
2026-03-17 06:12:42 -0400

    show the revision getting included

--- utilities/build.sh
+++ utilities/build.sh
@@ -2,4 +2,7 @@
 
 pushd gitrev; alr build; popd
 ./bin/gitrev -r 0.2.0 -eSHA=$GITHUB_SHA,RUN=$GITHUB_RUN_ID:$GITHUB_RUN_NUMBER -o revisions .
+cat revisions.ads
 pushd codemd; alr build; popd
+pushd dump ; alr build; popd
+pushd gitrev; alr clean; alr build; popd
