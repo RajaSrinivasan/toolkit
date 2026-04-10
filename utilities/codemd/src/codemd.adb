9f23079c5d2fb34225f01f0f3e6187780a660490
srinivasan <rajasrinivasan@hotmail.com>
2026-04-10 15:26:38 -0400

    process more than one input file

--- utilities/codemd/src/codemd.adb
+++ utilities/codemd/src/codemd.adb
@@ -14,25 +14,28 @@ begin
       return;
    end if;
 
-   declare
-      inpfile : constant String := cli.GetNextArgument;
-   begin
-      if inpfile'Length < 1 then
-         Put_Line ("Please provide an input file");
-         return;
-      end if ;
-      if cli.extractSection.all'Length > 1 then
-         impl.Extract
-            (inpfile, cli.extractSection.all, caption => cli.caption);
-      else
-         if cli.lineno_from > 0 then
+   loop
+      declare
+         inpfile : constant String := cli.GetNextArgument;
+      begin
+         if inpfile'Length < 1 then
+            pragma Debug (Put_Line ("Please provide an input file"));            
+            return;
+         end if ;
+         Put_Line ("Processing file " & inpfile);
+         if cli.extractSection.all'Length > 1 then
             impl.Extract
-               (inpfile, cli.lineno_from, cli.linecount, cli.caption);
+               (inpfile, cli.extractSection.all, caption => cli.caption);
          else
-            impl.Extract (inpfile, caption => cli.caption);
+            if cli.lineno_from > 0 then
+               impl.Extract
+                  (inpfile, cli.lineno_from, cli.linecount, cli.caption);
+            else
+               impl.Extract (inpfile, caption => cli.caption);
+            end if;
          end if;
-      end if;
-   end;
+      end;
+   end loop ;
 exception
    when GNAT.Command_Line.Exit_From_Command_Line =>
       null;
