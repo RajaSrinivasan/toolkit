5a638ae72a045e1d923f81cf5241388cb65f8ca3
srinivasan <rajasrinivasan@hotmail.com>
2026-04-12 06:54:05 -0400

    codemd output

--- utilities/revisions.ads
+++ utilities/revisions.ads
@@ -1,12 +1,12 @@
 --------------------------------------------
--- Created 2026-04-07 15:13:36
+-- Created 2026-04-12 06:52:41
 --------------------------------------------
 package revisions is
-	dir : constant String := "/Users/rajasrinivasan/Prj/GitHub/toolkit/utilities" ;
-	version : constant String := "0.3.0" ;
-	repo : constant String := "git@github.com:rajasrinivasan/toolkit.git" ;
-	commitid : constant String := "d757261892ccbdebe60185c9eacc97a7af474f09" ;
-	abbrev_commitid : constant String := "d757261" ;
-	tags : constant String := "0.1.0-dev" ;
-	branch : constant String := "codemd_quarto" ;
+	dir : constant String := "/home/srinivasan/Prj/GitHub/toolkit/utilities" ;
+	version : constant String := "0.3.1" ;
+	repo : constant String := "git@github.com:RajaSrinivasan/toolkit.git" ;
+	commitid : constant String := "483fde3bfbaee0a5c9f9ffe543b804d1d35827f4" ;
+	abbrev_commitid : constant String := "483fde3" ;
+	tags : constant String := "0.1.0-dev , 0.5.2 , v0.5 , v0.5.1 , v0.5.2" ;
+	branch : constant String := "main" ;
 end revisions ;
